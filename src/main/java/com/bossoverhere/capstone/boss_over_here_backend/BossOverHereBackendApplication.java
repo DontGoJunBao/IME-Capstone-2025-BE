48e1b24ad6bb178f420052bba8034e4dedd6b114
ykh4663 <superho7@gmail.com>
2025-05-13 17:04:52 +0900

    init setting

---
+++ src/main/java/com/bossoverhere/capstone/boss_over_here_backend/BossOverHereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bossoverhere.capstone.boss_over_here_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossOverHereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossOverHereBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bossoverhere/capstone/boss_over_here_backend/BossOverHereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bossoverhere.capstone.boss_over_here_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BossOverHereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
