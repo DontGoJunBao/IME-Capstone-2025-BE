b44775558d0412b5a63a87cfb86763ccf972eedc
ykh4663 <superho7@gmail.com>
2025-06-14 15:38:42 +0900

    feat : Oauth 관련 DTO들 추가

---
+++ src/main/java/com/bossoverhere/capstone/boss_over_here_backend/domain/auth/dto/response/OauthUserInfoResponse.java
@@ -0,0 +1,10 @@
+package com.bossoverhere.capstone.boss_over_here_backend.domain.auth.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class OauthUserInfoResponse {
+    private String id;
+}
