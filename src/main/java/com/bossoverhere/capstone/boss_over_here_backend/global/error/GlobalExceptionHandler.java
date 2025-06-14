e4e9a148f34dbf7d4b94793e9df0baa9facedfb3
ykh4663 <superho7@gmail.com>
2025-06-14 16:49:54 +0900

    feat: Response ErrorCode, OauthErrorCode 구현

--- src/main/java/com/bossoverhere/capstone/boss_over_here_backend/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/bossoverhere/capstone/boss_over_here_backend/global/error/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
