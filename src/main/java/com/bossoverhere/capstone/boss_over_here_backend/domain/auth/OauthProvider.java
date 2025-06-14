b5f2c9943782fed64d0e61a7539e0f0da56ac774
ykh4663 <superho7@gmail.com>
2025-06-14 15:29:06 +0900

    feat : OauthProvider 인터페이스 설계

---
+++ src/main/java/com/bossoverhere/capstone/boss_over_here_backend/domain/auth/OauthProvider.java
@@ -0,0 +1,17 @@
+package com.bossoverhere.capstone.boss_over_here_backend.domain.auth;
+
+import capston.junbao.BossOverHerePrac.domain.auth.domain.enums.OauthType;
+public interface OauthProvider {
+    OauthType getOauthType();
+    OauthClient getOAuthClient();
+
+    default boolean match(String providerName){
+        return providerName.equals(getOauthType().getTypeName());
+    }
+
+    default String getOAuthProviderUserId(String accessToken) {
+        OauthClient client = getOAuthClient();
+        return client.getOAuthProviderUserId(accessToken);
+    }
+
+}
