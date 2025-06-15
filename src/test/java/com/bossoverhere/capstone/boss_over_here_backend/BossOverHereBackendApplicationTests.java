96b69b31f08cc64f137f30ba7ebe1a4a113e531f
ykh4663 <superho7@gmail.com>
2025-06-15 13:15:57 +0900

    fix: pr-test 스크립트 (테스트 의존성 추가)

--- src/test/java/com/bossoverhere/capstone/boss_over_here_backend/BossOverHereBackendApplicationTests.java
+++ src/test/java/com/bossoverhere/capstone/boss_over_here_backend/BossOverHereBackendApplicationTests.java
@@ -3,9 +3,11 @@ package com.bossoverhere.capstone.boss_over_here_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("dev")
+@TestPropertySource(properties = "ai.server.url=http://172.16.21.144:8000/plan")
 class BossOverHereBackendApplicationTests {
 
 	@Test
